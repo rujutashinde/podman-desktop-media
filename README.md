8e2868f2e4228219b16522c4312bbe099a9eb82f
Rujuta Shinde <rushinde@redhat.com>
2026-03-19 16:24:08 -0400

    chore: removed other directories and files
    Signed-off-by: Rujuta Shinde <rushinde@redhat.com>

--- README.md
+++
@@ -1,2 +0,0 @@
-# podman-desktop-media
-Assets like videos pictures for Podman Desktop Release Notes, etc
--- videos/openvino/add-subtitles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ffmpeg \
-  -i openvino-podman-ai-lab.mp4 \
-  -vf subtitles=subtitles.srt \
-  openvino-podman-ai-lab-with-subtitles.mp4
